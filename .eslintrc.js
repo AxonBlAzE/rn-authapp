904233747e724f69b8ac74545d452bb2ee8b1e01
shreyans <shreyansms@gmail.com>
2025-01-24 20:41:49 -0500

    setting up workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native-community',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+  ],
+  rules: {
+    'react-native/no-inline-styles': 2,
+  },
 };
